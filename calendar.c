e1164dedb7495f78bde33d81a11277bce06e92d8
mrohith29 <mariyalarohith29@gmail.com>
2023-07-20 19:15:19 +0530

    .

---
+++ calendar.c
@@ -0,0 +1,24 @@
+// need to do some basic operations done using calender 
+#include<stdio.h>
+#include<stdlib.h>
+int main()
+{
+    int option;
+    while(1)
+    {
+        scanf("%d",&option);
+        switch(option)
+        {
+            case 1:
+            add();
+            break;
+
+            case 2:
+            search();
+            break;
+
+            case 3:
+            
+        }
+    }
+}
