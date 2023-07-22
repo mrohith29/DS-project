b241a769f5fe6d4508cee62c6c115abc85081d3a
mrohith29 <mariyalarohith29@gmail.com>
2023-07-22 17:19:34 +0530

    started doing AddTask function.

--- calendar.c
+++ calendar.c
@@ -1,30 +1,41 @@
 // need to do some basic operations done using calender 
 #include<stdio.h>
 #include<stdlib.h>
+#include<string.h>
 
-struct task
-{
-    struct date
+struct time
     {
-        int date;
+        int month;
+        int year;
+        int Date;
         int hours;
         int minutes;
     };
-    char content;
+
+struct task
+{
+    struct time *initial;
+    char *content;
     struct task *prev;
     struct task *next;
 }*start = NULL;
 
-struct task *AddTask()
+struct task *AddTask(struct task *start, int date, int month, int year, int hours, int min, char *theme)
 {
-
+    if(start == NULL)
+    {
+        struct task *newtask = (struct task *)malloc(sizeof(struct task));
+        newtask->initial = (struct time *)malloc(sizeof(struct time));
+        
+    }
 }
 
 
 void main()
 {
 
-    int date, month, year, hours, minutes, option; 
+    int date, month, year, hours, min, option;
+    char theme[100];  
     while(1)
     {
         printf("*****TODAY'S DATE   ");
@@ -43,14 +54,14 @@ void main()
             scanf("%d",&month);
             scanf("%d",&year);
             scanf("%d",&hours);
-            scanf("%d",&minutes);
-                start = AddTask(start, date, hours, minutes);
+            scanf("%d",&min);
+            getchar();
+            fgets(theme, sizeof(theme), stdin);
+            printf("%d  %d  %d  %d  %d \n",date,month,year,hours,min);
+            printf("%s\n",theme);
+                start = AddTask(start, date, month, year, hours, min, theme);
 
             break;
         }
-
-
-    }
-
-    
+    }  
 }
