3dba040d49380313e0c1818c3541f301bbaef0a3
mrohith29 <mariyalarohith29@gmail.com>
2023-07-22 21:43:14 +0530

    updated the present code.

--- calendar.c
+++ calendar.c
@@ -1,16 +1,16 @@
-// need to do some basic operations done using calender 
-#include<stdio.h>
-#include<stdlib.h>
-#include<string.h>
+// need to do some basic operations done using calender
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 struct time
-    {
-        int month;
-        int year;
-        int Date;
-        int hours;
-        int minutes;
-    };
+{
+    int month;
+    int year;
+    int Date;
+    int hours;
+    int minutes;
+};
 
 struct task
 {
@@ -18,11 +18,11 @@ struct task
     char *content;
     struct task *prev;
     struct task *next;
-}*start = NULL;
+} *start = NULL;
 
 struct task *AddTask(struct task *start, int date, int month, int year, int hours, int min, char *theme)
 {
-    if(start == NULL)
+    if (start == NULL)
     {
         struct task *newtask = (struct task *)malloc(sizeof(struct task));
         newtask->initial = (struct time *)malloc(sizeof(struct time));
@@ -31,30 +31,32 @@ struct task *AddTask(struct task *start, int date, int month, int year, int hour
         newtask->initial->minutes = min;
         newtask->initial->month = month;
         newtask->initial->year = year;
-        newtask->content = (char *)malloc(strlen((theme)+1)*sizeof(char));
+        newtask->content = (char *)malloc(strlen((theme) + 1) * sizeof(char));
         strcpy(start->content, theme);
         newtask->next = newtask->prev = NULL;
         return newtask;
     }
     else if (year < start->initial->year || (year == start->initial->year && month < start->initial->month) ||
-        (year == start->initial->year && month == start->initial->month && date == start->initial->Date && hours < start->initial->hours) ||
-        (year == start->initial->year && month == start->initial->month && date < start->initial->Date) ||
-        (year == start->initial->year && month == start->initial->month && date == start->initial->Date && hours == start->initial->hours && min < start->initial->minutes)) {
+             (year == start->initial->year && month == start->initial->month && date == start->initial->Date && hours < start->initial->hours) ||
+             (year == start->initial->year && month == start->initial->month && date < start->initial->Date) ||
+             (year == start->initial->year && month == start->initial->month && date == start->initial->Date && hours == start->initial->hours && min < start->initial->minutes))
+    {
         start->prev = AddTask(start->prev, date, month, year, hours, min, theme);
-    } else {
+    }
+    else
+    {
         start->next = AddTask(start->next, date, month, year, hours, min, theme);
     }
 
-    return ;
+    return start;
 }
-
 
 void main()
 {
 
     int date, month, year, hours, min, option;
-    char theme[100];  
-    while(1)
+    char theme[100];
+    while (1)
     {
         printf("*****TODAY'S DATE   ");
         printf(__DATE__);
@@ -64,22 +66,28 @@ void main()
         printf(__TIME__);
         printf("*****\n");
 
-        scanf("%d",&option);
-        switch(option)
+        scanf("%d", &option);
+        switch (option)
         {
-            case 1: 
-            scanf("%d",&date);
-            scanf("%d",&month);
-            scanf("%d",&year);
-            scanf("%d",&hours);
-            scanf("%d",&min);
+        case 1:
+            scanf("%d", &date);
+            scanf("%d", &month);
+            scanf("%d", &year);
+            scanf("%d", &hours);
+            scanf("%d", &min);
             getchar();
             fgets(theme, sizeof(theme), stdin);
-            printf("%d  %d  %d  %d  %d \n",date,month,year,hours,min);
-            printf("%s\n",theme);
-                start = AddTask(start, date, month, year, hours, min, theme);
-
+            // printf("%d  %d  %d  %d  %d \n", date, month, year, hours, min);
+            // printf("%s\n", theme);
+            start = AddTask(start, date, month, year, hours, min, theme);
             break;
+
+        case 2:
+        exit(0);
+
+        default:
+        printf("Enter a Valid operation\n");
+        break;
         }
-    }  
+    }
 }
